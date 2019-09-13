00f1168825c83b1728535c8bf1f21c37cd64ead0
Jathurshan <s.jathur93@gmail.com>
2019-09-14 00:59:53 +0530

    springmvc_crud

---
+++ src/main/java/com/app/model/Emp.java
@@ -0,0 +1,33 @@
+package com.app.model;
+
+public class Emp {
+	public int id;
+	public String name;
+	public int salary;
+	public String designation;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
